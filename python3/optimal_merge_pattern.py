679867fc0a901f4d8e20ec5d590ebd65874825ae
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-05-12 21:59:29 +0530

    Create optimal_merge_pattern.py

---
+++ python3/optimal_merge_pattern.py
@@ -0,0 +1,14 @@
+def Optimalmerge(arr):
+    arr.sort()
+    count=0
+    while len(arr)>1:
+        a=min(arr)
+        arr.remove(a)
+        b=min(arr)
+        arr.remove(b)
+        count+=(a+b)
+        arr.append(a+b)
+        print(arr)
+    return count
+        
+Optimalmerge([2, 3, 4, 5, 6, 7])
